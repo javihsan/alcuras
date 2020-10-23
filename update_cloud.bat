5053d0ebbe9099f9be41f7fe9858e0cb328ed88d
javihsan <javihsan@gmail.com>
2020-10-23 12:55:43 +0200

    gcloud refininado

---
+++ update_cloud.bat
@@ -0,0 +1,5 @@
+SET PATH=C:\GCloudSDK\google-cloud-sdk\bin;C:\openjdk_1.8.0\bin;%PATH%;
+mvn clean package -P prod
+gcloud config set project alcuras-web
+gcloud app deploy .\target\alcurasweb\WEB-INF\appengine-web.xml --version=r8-0-5 --no-stop-previous-version --no-promote
+
