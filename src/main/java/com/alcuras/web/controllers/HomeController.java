5e45ffd90deaa97105b035668f60c4a47cdfd973
Francisco Javier Sánchez Barrado <fjsanchez@atsistemas.com>
2024-11-06 16:30:58 +0100

    index yalm

--- src/main/java/com/alcuras/web/controllers/HomeController.java
+++ src/main/java/com/alcuras/web/controllers/HomeController.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
-//@RequestMapping(value={"/*inicio*", "/", ""})
 public class HomeController {
 	  
 	private String view;
--- update_cloud.bat
+++ update_cloud.bat
@@ -1,12 +1,9 @@
 SET PATH=C:\GCloudSDK\google-cloud-sdk\bin;C:\openjdk_1.8.0\bin;%PATH%;
 
-mvn clean package -P prod
-gcloud config set project alcurasweb
-gcloud app deploy .\target\alcurasweb\WEB-INF\appengine-web.xml --version=r8-0-0 --no-stop-previous-version --no-promote
-gcloud app deploy .\target\alcurasweb\WEB-INF\cron.yaml
-
-new java21:
 gcloud config set project alcurasweb
 mvn package appengine:deploy -P prod
 
 gcloud beta app migrate-config datastore-indexes-xml-to-yaml src\main\webapp\WEB-INF\datastore-indexes.xml
+mvn clean package appengine:deployIndex
+
+gcloud app deploy .\target\alcurasweb\WEB-INF\cron.yaml
